be26c0118dd24bcfef898d772947b7ff9dd7957c
Gustaf Dalemar <gustaf.dalemar@gmail.com>
2016-04-01 19:00:50 +0200

    Fixed incorrect runHookDirectly signature

--- packages/roc-plugin-start/src/helpers/init-runtime.js
+++ packages/roc-plugin-start/src/helpers/init-runtime.js
@@ -3,7 +3,10 @@ import { name } from '../roc/util';
 
 export default function initRuntime() {
     // Init additional runtimes
-    runHookDirectly(name, 'register-runtime');
+    runHookDirectly({
+        extension: name,
+        name: 'register-runtime'
+    });
 
-    console.log('Roc runtime has been initialized');
+    console.log('Roc runtime has been initialized.');
 }
