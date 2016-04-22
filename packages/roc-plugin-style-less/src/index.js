0bfb808ea8e2074be3fc5098c37cca1d73fbfdae
Christian Lizell <christian.lizell@aftonbladet.se>
2016-04-22 11:04:09 +0200

    Removed unnecessary, empty configuration

--- packages/roc-plugin-style-less/src/index.js
+++ packages/roc-plugin-style-less/src/index.js
@@ -1,27 +1,5 @@
 export const roc = {
     name: require('../package.json').name,
-    config: {
-        settings: {
-            build: {
-                less: {}
-            }
-        }
-    },
-    meta: {
-        settings: {
-            descriptions: {
-                build: {
-                    less: {}
-                }
-            },
-
-            validations: {
-                build: {
-                    less: {}
-                }
-            }
-        }
-    },
     actions: {
         less: {
             extension: 'roc-plugin-style-css',
