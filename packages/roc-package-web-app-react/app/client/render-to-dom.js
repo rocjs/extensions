7f44273fc814cf1e0c82188b4e0d5f57b7e92db9
Gustaf Dalemar <gustaf.dalemar@gmail.com>
2017-10-25 16:02:02 +0100

    feat(MULTI): Support React 15 and 16, and making it a peerDependency
    This is a work in progress and more work is needed for an optimal implementation.
    
    - There is a problem with Redux Dev Tools that makes React show a warning.
    - A warning from Roc will be seen, this can be ignored and will be removed. Currently used to get npm link to work during local development.
    
    BREAKING CHANGES: Projects will now need to install React manually in their projects. `npm install --save react react-dom prop-types`
    SCOPES: roc-plugin-react, roc-package-web-app-react

--- packages/roc-package-web-app-react/app/client/render-to-dom.js
+++ packages/roc-package-web-app-react/app/client/render-to-dom.js
@@ -1,8 +1,9 @@
-/* eslint-disable global-require */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from 'react-router/lib/Router';
 import match from 'react-router/lib/match';
+
+import { rocConfig } from '../shared/universal-config';
 
 function renderSync({ renderProps, createComponent, routerRenderFn }, node) {
     const finalComponent = createComponent(
@@ -12,7 +13,12 @@ function renderSync({ renderProps, createComponent, routerRenderFn }, node) {
         />
     );
 
-    ReactDOM.render(finalComponent, node);
+    // If we have enabled SSR and we are on React 16
+    if (rocConfig.runtime.ssr && ReactDOM.hydrate) {
+        ReactDOM.hydrate(finalComponent, node);
+    } else {
+        ReactDOM.render(finalComponent, node);
+    }
 }
 
 export default function renderAsync({ history, routes, ...rest }, node) {
--- plugins/roc-plugin-react/src/roc/index.js
+++ plugins/roc-plugin-react/src/roc/index.js
@@ -1,13 +1,29 @@
-import { generateDependencies } from 'roc';
-
-const packageJSON = require('../../package.json');
+import path from 'path';
 
 export default {
     dependencies: {
-        exports: generateDependencies(packageJSON, [
-            'prop-types',
-            'react',
-            'react-dom',
-        ]),
+        exports: {
+            // TODO
+            // This is currently here to manage a "problem" when working with locally linked packages
+            // This will give a warning the the projects that use this plugin
+            // We want to change core to support this in a better way in the future.
+            // One way could be to no give warnings if the version is undefined/false
+            // Another could be to let Roc be smart about this and automatically solve the
+            // peerDependecy problems that exists in Node and that we here go around
+            // Another useful thing is to get the context.directory here directly instead of needing
+            // to use process.cwd()
+            react: {
+                version: 'IGNORE THE WARNING THAT YOU SEE',
+                resolve: ({ request }) => require.resolve(path.join(process.cwd(), 'node_modules', request)),
+            },
+            'react-dom': {
+                version: 'IGNORE THE WARNING THAT YOU SEE',
+                resolve: ({ request }) => require.resolve(path.join(process.cwd(), 'node_modules', request)),
+            },
+        },
+        requires: {
+            react: '^15.5.4 || ^16.0.0',
+            'react-dom': '^15.5.4 || ^16.0.0',
+        },
     },
 };
